5424a54b9695bf19bc4fc487f8990eca44040687
Aleksander <aleksander.m.piekarz@gmail.com>
2019-06-23 09:34:46 +0200

    Initial commit

---
+++ src/main/java/aleksander/App.java
@@ -0,0 +1,38 @@
+package aleksander;
+
+
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[] {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+        //webapp.addServlet(HelloServ0let.class,"/api/*")
+        var server = new org.eclipse.jetty.server.Server(8080);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
+
+    }
+}
